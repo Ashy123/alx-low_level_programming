106f93db5845c1606729d2a5547d1ead36a97c04
Ashy123 <ashwinny76@gmail.com>
2022-10-25 23:18:05 +0000

    Task 9

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
-/**
- * _strcpy - copy the string pointed to by src to dest
- * @dest: char to check
- * @src: char to check
- * Return: 0 is success
- */
-char *_strcpy(char *dest, char *src)
-{
-	int a;
+#include <stdio.h>
 
-	for (a = 0; src[a] != '\0'; a++)
-		dest[a] = src[a];
-	dest[a] = '\0';
-	return (dest);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98];
+    char *ptr;
+
+    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", ptr);
+    return (0);
 }
